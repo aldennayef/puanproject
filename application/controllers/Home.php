35d29ce159d1a8d9dbe6d2ced855cbd93a588303
aldennayef <aldennayef@gmail.com>
2023-05-23 20:30:15 +0700

    commit on main

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class index extends CI_Controller {
+class Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,6 @@ class index extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('index');
 	}
 }
